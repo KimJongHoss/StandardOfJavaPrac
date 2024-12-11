92e1cec0dce91ad642c4a1d3a0cb547fb883de06
KimJongHoss <haberde97@gmail.com>
2024-12-11 10:10:47 +0900

    variable practice

---
+++ studyJava/src/testSearch/AnnotationTest.java
@@ -0,0 +1,19 @@
+package testSearch;
+
+/*
+ * Date : 2024.12.13
+ * Source : AnnotationTest.java
+ * Author : 진득코딩
+ * Email : jinco@gmail.com
+ */
+
+public class AnnotationTest { 
+	
+	public static void main(String[] arg) { 
+			System.out.println("Hello, /* 이것은 주석 아님 */ world.");
+			System.out.println("Hello,world. // 이것도 주석 아님");
+	}
+
+}
+
+
