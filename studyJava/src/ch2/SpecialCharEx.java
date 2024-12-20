8e19e2a47ee06e4fdaac47b92dba4361dc75a847
KimJongHoss <haberde97@gmail.com>
2024-12-20 12:15:19 +0900

    Practice primitive type1

---
+++ studyJava/src/ch2/SpecialCharEx.java
@@ -0,0 +1,13 @@
+package ch2;
+
+public class SpecialCharEx {
+	
+	public static void main(String[] args) {
+		System.out.println('\''); // '''처럼 할 수 없다.
+		System.out.print("abc\t123\b456"); //\b에 의해 3이 지워진다.
+		System.out.println('\n'); // 개행(new line) 문자 출력하고 개행
+		System.out.println("\"Hello\""); // 큰따옴표를 출력하는 방법
+		System.out.println("c:\\");
+	}
+
+}
